ca680a8763393e7407d42ae8ac211cca2ee41c23
SumitavaBiswas <myuseless15@gmail.com>
2023-04-10 03:35:18 +0530

    Adding database and controller

--- src/main/java/com/habit/tracker/model/DailyLog.java
+++ src/main/java/com/habit/tracker/model/DailyLog.java
@@ -1,10 +1,10 @@
 package com.habit.tracker.model;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.util.Set;
 
 @Entity
--- src/main/java/com/habit/tracker/model/Habit.java
+++ src/main/java/com/habit/tracker/model/Habit.java
@@ -3,11 +3,7 @@ package com.habit.tracker.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 @Getter
