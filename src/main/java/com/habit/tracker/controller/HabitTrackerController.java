64a20b7d2aef803a20829fa70a9a0746d3eef2e3
SumitavaBiswas <myuseless15@gmail.com>
2023-04-15 14:14:00 +0530

    Making post request from UI

--- src/main/java/com/habit/tracker/controller/HabitTrackerController.java
+++ src/main/java/com/habit/tracker/controller/HabitTrackerController.java
@@ -3,16 +3,14 @@ package com.habit.tracker.controller;
 import com.habit.tracker.model.Habit;
 import com.habit.tracker.service.HabitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HabitTrackerController {
     @Autowired
     HabitService habitService;
 
+    @CrossOrigin
     @PostMapping("/habit")
     public void addHabit(@RequestBody Habit habit){
         habitService.saveHabit(habit);
--- src/test/java/com/habit/tracker/controller/HabitTrackerControllerTest.java
+++ src/test/java/com/habit/tracker/controller/HabitTrackerControllerTest.java
@@ -1,9 +1,11 @@
 package com.habit.tracker.controller;
 
+import com.habit.tracker.service.HabitService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +20,9 @@ class HabitTrackerControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    HabitService habitService;
 
     @Test
     public void testSaveHabit() throws Exception {
